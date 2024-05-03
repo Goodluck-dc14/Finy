90a9a4cc59e63ad5e746721003472f42ec6df172
Goodluck <84187549+Goodluck-dc14@users.noreply.github.com>
2024-05-03 19:36:56 +0100

    Update bankStatementController.js

--- controllers/bankStatementController.js
+++ controllers/bankStatementController.js
@@ -1,9 +1,24 @@
 const cloudinary = require("../utils/cloudinary");
 const BankStatement = require("../models/bankStatement");
+const { generateResponse } = require("../services/geminiService");
+const mime = require('mime-types');
+const fs = require('fs');
+
+
+async function fileToGenerativePart(path) {
+  const mimeType = mime.lookup(path) || 'application/octet-stream';
+  const data = await fs.promises.readFile(path); // Read file contents using promises
+  return {
+    inlineData: {
+      data: data.toString("base64"),
+      mimeType
+    },
+  };
+}
 
 exports.uploadBankStatement = async (req, res) => {
   try {
-    const { statementData } = req.body;
+    const { statementData, message } = req.body;
 
     if (!statementData) {
       return res.status(400).json({ error: "statementData is required" });
@@ -15,6 +30,9 @@ exports.uploadBankStatement = async (req, res) => {
         .json({ error: "Bank statement image is required" });
     }
 
+    const myImage = [ await fileToGenerativePart(req.file.path) ];
+    const response = await generateResponse(myImage, message);
+
     const result = await cloudinary.uploads(req.file.path, "image");
 
     const bankStatement = new BankStatement({
@@ -24,9 +42,22 @@ exports.uploadBankStatement = async (req, res) => {
 
     await bankStatement.save();
 
-    res.status(201).json({ message: "Bank statement uploaded successfully" });
+    res.status(201).json({ message: "Bank statement uploaded successfully", data: response });
   } catch (error) {
     console.error("Error uploading bank statement:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+exports.getAllBankStatement = async (req, res) => {
+  try {
+    const bankStatements = await BankStatement.find();
+
+    res.status(200).json({
+      status: "Success",
+      data: bankStatements
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
