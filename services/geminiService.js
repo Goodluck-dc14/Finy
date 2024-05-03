ca67b84380c7667a4b3fc0343a8ccbb996097bda
Goodluck <84187549+Goodluck-dc14@users.noreply.github.com>
2024-05-03 19:43:26 +0100

    Update geminiService.js

--- services/geminiService.js
+++ services/geminiService.js
@@ -4,11 +4,13 @@ const { GOOGLE_API_KEY } = require("../config");
 const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 
 module.exports = {
-  async generateResponse(query) {
+  async generateResponse(imageParts, request) {
     try {
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
-      const result = await model.generateContent(query);
+      const prompt = await request;
+      console.log(imageParts);
+      const result = await model.generateContent([ prompt, ...imageParts ]);
       const response = await result.response;
       const text = response.text();
 
